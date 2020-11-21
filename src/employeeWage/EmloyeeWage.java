b922897f8717c166004a62c96b6903828a34640f
unknown <nituldeori63@gmail.com>
2020-11-21 13:15:16 +0530

    Added part time employee wage

--- src/employeeWage/EmloyeeWage.java
+++ src/employeeWage/EmloyeeWage.java
@@ -3,12 +3,15 @@ package employeeWage;
 public class EmloyeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
-		int is_full_time=1;
+		int is_part_time=1;
+		int is_full_time=2;
 		int emp_rate_per_hour=20;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck=Math.floor(Math.random()*10)%2;
-		if(empCheck==is_full_time)
+		double empCheck=Math.floor(Math.random()*10)%3;
+		if(empCheck==is_part_time)
+			empHrs=4;
+		else if(empCheck==is_full_time)
 			empHrs=8;
 		else
 			empHrs=0;
