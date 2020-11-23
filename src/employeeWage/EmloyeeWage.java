1aa96de27ed0df2f4d75b633592993b71f5ca95a
unknown <nituldeori63@gmail.com>
2020-11-23 23:35:20 +0530

    Wriiten a Class Method to compute employee wage

--- src/employeeWage/EmloyeeWage.java
+++ src/employeeWage/EmloyeeWage.java
@@ -1,37 +1,43 @@
 package employeeWage;
 
 public class EmloyeeWage {
-	public static final int is_part_time=1;
-	public static final int is_full_time=2;
-	public static final int emp_rate_per_hour=20;
-	public static final int num_of_working_days=20;
-	public static final int max_hrs_in_month=100;
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation");
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
+	public static final int EMP_RATE_PER_HOUR=20;
+	public static final int NUM_OF_WORKING_DAYS=20;
+	public static final int MAX_HRS_IN_MONTH=100;
+	public static int computeEmpWage() {
+        System.out.println("Welcome to Employee Wage Computation");
 		
 		int empHrs=0;
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
 		int empCheck=(int)Math.floor(Math.random()*10)%3;
-		while(totalEmpHrs<=max_hrs_in_month && totalWorkingDays<num_of_working_days) {
+		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
 			
 			
 			switch(empCheck) {
-			case is_part_time:
-				empHrs=4;
-				break;
-			case is_full_time:
-				empHrs=8;
-				break;
-			default:
-				empHrs=0;
+			    case IS_PART_TIME:
+				    empHrs=4;
+				    break;
+			    case IS_FULL_TIME:
+				    empHrs=8;
+				    break;
+			    default:
+				    empHrs=0;
 			}
 			totalEmpHrs+=empHrs;
 			totalWorkingDays++;
 		}
 		System.out.println("Working Days: "+totalWorkingDays+" Emp Hrs: "+totalEmpHrs);
-		int totalEmpWage=totalEmpHrs*emp_rate_per_hour;
+		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: "+totalEmpWage);
+		return totalEmpWage;
+		
 	}
+	public static void main(String[] args) {
+		computeEmpWage();
+	}
+		
 
 }
