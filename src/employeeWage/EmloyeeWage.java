2e9b02002850048da3abc051cff9165c35ec872f
unknown <nituldeori63@gmail.com>
2020-11-24 01:00:30 +0530

    Compute Employee wage and save Employee Wage by Company

--- src/employeeWage/EmloyeeWage.java
+++ src/employeeWage/EmloyeeWage.java
@@ -3,7 +3,18 @@ package employeeWage;
 public class EmloyeeWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static int computeEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
+	private final String company;
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
+	public EmloyeeWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth) {
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxHoursPerMonth=maxHoursPerMonth;
+	}
+	public void computeEmpWage() {
         System.out.println("Welcome to Employee Wage Computation");
 		
 		int empHrs=0;
@@ -28,14 +39,15 @@ public class EmloyeeWage {
 			System.out.println("Day#"+totalWorkingDays+" Emp Hr: "+empHrs);
 		}
 		System.out.println("Working Days: "+totalWorkingDays+" Emp Hrs: "+totalEmpHrs);
-		int totalEmpWage=totalEmpHrs*empRatePerHour;
+		totalEmpWage=totalEmpHrs*empRatePerHour;
 		System.out.println("Total Emp Wage for Company "+company+" is: "+totalEmpWage);
-		return totalEmpWage;
 		
 	}
 	public static void main(String[] args) {
-		computeEmpWage("DMart",20,2,10);
-		computeEmpWage("Reliance",10,4,20);
+		EmloyeeWage dmart=new EmloyeeWage("DMart",20,2,10);
+		EmloyeeWage reliance=new EmloyeeWage("Reliance",10,4,20);
+		dmart.computeEmpWage();
+		reliance.computeEmpWage();
 	}
 		
 
