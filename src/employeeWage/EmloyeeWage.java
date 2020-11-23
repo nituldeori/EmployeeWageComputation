4290a2185080d326a1cd7a274a533c4e6d099630
unknown <nituldeori63@gmail.com>
2020-11-24 00:39:23 +0530

    Ability to compute employee wage for multiple companies

--- src/employeeWage/EmloyeeWage.java
+++ src/employeeWage/EmloyeeWage.java
@@ -3,9 +3,6 @@ package employeeWage;
 public class EmloyeeWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int NUM_OF_WORKING_DAYS=20;
-	public static final int MAX_HRS_IN_MONTH=100;
 	public static int computeEmpWage(String company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth) {
         System.out.println("Welcome to Employee Wage Computation");
 		
@@ -13,7 +10,7 @@ public class EmloyeeWage {
 		int totalEmpHrs=0;
 		int totalWorkingDays=0;
 		
-		while(totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+		while(totalEmpHrs<=maxHoursPerMonth&& totalWorkingDays<numOfWorkingDays) {
 			int empCheck=(int)Math.floor(Math.random()*10)%3;
 			
 			switch(empCheck) {
@@ -31,7 +28,7 @@ public class EmloyeeWage {
 			System.out.println("Day#"+totalWorkingDays+" Emp Hr: "+empHrs);
 		}
 		System.out.println("Working Days: "+totalWorkingDays+" Emp Hrs: "+totalEmpHrs);
-		int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+		int totalEmpWage=totalEmpHrs*empRatePerHour;
 		System.out.println("Total Emp Wage for Company "+company+" is: "+totalEmpWage);
 		return totalEmpWage;
 		
