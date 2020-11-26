53229ac24c8456a1bdbae9a5ebde0d82fde44052
unknown <nituldeori63@gmail.com>
2020-11-26 17:11:29 +0530

    Added code to get total wage when queried by company

--- src/employeeWage/EmpWageBuilderArray.java
+++ src/employeeWage/EmpWageBuilderArray.java
@@ -1,6 +1,8 @@
 package employeeWage;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class EmpWageBuilderArray implements IComputeEmpWage {
 	public static final int IS_PART_TIME=1;
@@ -8,14 +10,16 @@ public class EmpWageBuilderArray implements IComputeEmpWage {
 	
 	private int numOfCompany=0;
 	private LinkedList<CompanyEmpWage> companyEmpWageList;
-	
+	private Map<String,CompanyEmpWage> companyToEmpWageMap;
 	public EmpWageBuilderArray() {
 		companyEmpWageList=new LinkedList<>();
+		companyToEmpWageMap=new HashMap<>();
 	}
 	
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
 		CompanyEmpWage companyEmpWage=new CompanyEmpWage(company, empRatePerHour,numOfWorkingDays, maxHoursPerMonth);
 		companyEmpWageList.add(companyEmpWage);
+		companyToEmpWageMap.put(company, companyEmpWage);
 	}
 	
 	public void computeEmpWage() {
@@ -26,6 +30,10 @@ public class EmpWageBuilderArray implements IComputeEmpWage {
 		}
 	}
 	
+	@Override
+	public int getTotalWage(String company) {
+		return companyToEmpWageMap.get(company).totalEmpWage;
+	}
 	private int computeEmpWage(CompanyEmpWage companyEmpWage) {
 		
 		int empHrs=0, totalEmpHrs=0, totalWorkingDays=0;
@@ -61,6 +69,7 @@ public class EmpWageBuilderArray implements IComputeEmpWage {
 		empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
 		empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
 		empWageBuilder.computeEmpWage();
+		System.out.println("Total Wage for DMart Company: "+empWageBuilder.getTotalWage("DMart"));
 	}
 
 }
--- src/employeeWage/IComputeEmpWage.java
+++ src/employeeWage/IComputeEmpWage.java
@@ -3,5 +3,6 @@ package employeeWage;
 public interface IComputeEmpWage {
 	public void addCompanyEmpWage(String company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 
 }
