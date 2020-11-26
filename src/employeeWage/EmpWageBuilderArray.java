6b794b4003f3699854b51ba5f3ad524d03a87be8
unknown <nituldeori63@gmail.com>
2020-11-26 16:40:37 +0530

    Refactored code to have list o multiple companies to manage Employee Wage

--- src/employeeWage/EmpWageBuilderArray.java
+++ src/employeeWage/EmpWageBuilderArray.java
@@ -1,25 +1,28 @@
 package employeeWage;
 
+import java.util.LinkedList;
+
 public class EmpWageBuilderArray implements IComputeEmpWage {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	
 	private int numOfCompany=0;
-	private CompanyEmpWage[] companyEmpWageArray;
+	private LinkedList<CompanyEmpWage> companyEmpWageList;
 	
 	public EmpWageBuilderArray() {
-		companyEmpWageArray=new CompanyEmpWage[5];
+		companyEmpWageList=new LinkedList<>();
 	}
 	
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
-		companyEmpWageArray[numOfCompany]=new CompanyEmpWage(company, empRatePerHour,numOfWorkingDays, maxHoursPerMonth);
-		numOfCompany++;
+		CompanyEmpWage companyEmpWage=new CompanyEmpWage(company, empRatePerHour,numOfWorkingDays, maxHoursPerMonth);
+		companyEmpWageList.add(companyEmpWage);
 	}
 	
 	public void computeEmpWage() {
-		for(int i=0;i<numOfCompany;i++) {
-			companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-			System.out.println(companyEmpWageArray[i]);
+		for(int i=0;i<companyEmpWageList.size();i++) {
+			CompanyEmpWage companyEmpWage=companyEmpWageList.get(i);
+			companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+			System.out.println(companyEmpWage);
 		}
 	}
 	
